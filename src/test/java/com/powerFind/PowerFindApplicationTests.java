c83c05a3f8f288733f98f9a1a259538b782b631e
harut <htarzyanh@gmail.com>
2025-05-29 17:31:12 +0400

    init of project

---
+++ src/test/java/com/powerFind/PowerFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerFind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
